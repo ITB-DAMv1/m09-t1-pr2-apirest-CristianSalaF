1412baad503e04e36900adf26bb3d5523e955fdd
Csf91 <contact@csf91.dev>
2025-04-18 18:16:57 +0200

    Client authentication implementation, and connection to the API, #21

---
+++ m09-t1-pr2-apirest-CristianSalaF/T1PR2-Client/Model/LoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace T1PR2_Client.Model
+{
+    public class LoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ m09-t1-pr2-apirest-CristianSalaF/T1PR2-Client/Tools/TokenHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace T1PR2_Client.Tools
+{
+    public static class TokenHelper
+    {
+        public static bool IsTokenSession(string token)
+        {
+            return !string.IsNullOrEmpty(token) && !IsTokenExpired(token);
+        }
+        /// <summary>
+        /// Validar si el token ha expirat o no
+        /// Requereix de la instal·lacio de la llibreria System.IdentityModel.Tokens.Jwt
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static bool IsTokenExpired(string token)
+        {
+            // Extract the token if it's wrapped in a JSON object
+            if (token.StartsWith("{") && token.Contains("\"token\":"))
+            {
+                var json = System.Text.Json.JsonDocument.Parse(token);
+                token = json.RootElement.GetProperty("token").GetString();
+            }
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(token);
+            var expiration = jwt.ValidTo;
+            return expiration < DateTime.UtcNow;
+        }
+    }
+}
